b8eddf37221af91f6659a83f5482e58bf9bbbe04
anbunathan <anbunathan.r@gmail.com>
2020-03-23 22:28:55 +0530

    Bookworm Tutorial1 commit: Client side - Bookworm Top Navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from "react-redux";
 import {Route} from 'react-router-dom';
 import PropTypes from "prop-types";
 import HomePage from './components/pages/HomePage';
@@ -7,12 +8,15 @@ import DashboardPage from './components/pages/DashboardPage';
 import SignupPage from './components/pages/SignupPage';
 import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
 import ResetPasswordPage from "./components/pages/ResetPasswordPage";
+
 import ConfirmationPage from "./components/pages/ConfirmationPage";
 import UserRoute from "./components/routes/UserRoute";
 import GuestRoute from "./components/routes/GuestRoute";
+import TopNavigation from "./components/navigation/TopNavigation";
 
-const App = ({location}) => 
+const App = ({location, isAuthenticated}) =>   
   <div className="ui container">
+    {isAuthenticated && <TopNavigation />}
     <Route location={location} path="/" exact component={HomePage}/>
     <GuestRoute location={location} path="/login" exact component={LoginPage}/>
     <GuestRoute location={location} path="/signup" exact component={SignupPage}/>
@@ -35,7 +39,12 @@ const App = ({location}) =>
       component={ConfirmationPage}
     />
     
-    <UserRoute location={location} path="/dashboard" exact component={DashboardPage}/>
+    <UserRoute 
+      location={location} 
+      path="/dashboard" 
+      exact 
+      component={DashboardPage}/>
+    
   </div>
 
 
@@ -43,7 +52,13 @@ App.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired
   }).isRequired,
-  
+  isAuthenticated: PropTypes.bool.isRequired
 };
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.user.email
+  };
+}
+
+export default connect(mapStateToProps)(App);
--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -2,24 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import ConfirmEmailMessage from "../messages/ConfirmEmailMessage";
+import { allBooksSelector } from "../../reducers/books";
+import AddBookCtA from "../ctas/AddBookCtA";
 
-const DashboardPage = ({isConfirmed}) => {
+const DashboardPage = ({isConfirmed, books}) => {
     return (
         <div>
             {!isConfirmed && <ConfirmEmailMessage />}
+            {books.length === 0 ? <AddBookCtA /> : <p>You have books!</p>}
         </div>
     );
 };
 
 DashboardPage.propTypes = {
-  isConfirmed: PropTypes.bool.isRequired
-  
+  isConfirmed: PropTypes.bool.isRequired,
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    isConfirmed: !!state.user.confirmed
-    
+    isConfirmed: !!state.user.confirmed,
+    books: allBooksSelector(state)
   };
 }
 
